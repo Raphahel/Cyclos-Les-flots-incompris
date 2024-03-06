9a3d4b370e8437256117e93cee876f6c5f73bbc7
antoine-belliard <60436836+antoine-belliard@users.noreply.github.com>
2024-03-06 17:02:14 +0100

    Tempete continuelle

--- Assets/Scripts/UmibozuAI/FollowBezierCurve.cs
+++ Assets/Scripts/UmibozuAI/FollowBezierCurve.cs
@@ -7,24 +7,25 @@ using UnityEngine.AI;
 public class FollowBezierCurve : MonoBehaviour
 {
     [SerializeField] private Transform[] routes;
-    private int routeToGo;
+    //private int routeToGo;
     private float tParam;
     private Vector3 objectPosition;
     [SerializeField] private float speedModifier;
-    private bool coroutineAllowed;
+    //private bool coroutineAllowed;
 
     private float radius;
 
     void Start()
 
     {
-        routeToGo = 0;
+        //routeToGo = 0;
         tParam = 0f;
-        coroutineAllowed = true;
+        //coroutineAllowed = true;
         radius = 100f;
+        StartCoroutine(GoByTheRoute());
     }
 
-    void Update()
+    /*void Update()
 
     {
         if (coroutineAllowed)
@@ -32,24 +33,18 @@ public class FollowBezierCurve : MonoBehaviour
             StartCoroutine(GoByTheRoute(routeToGo));
         }
 
-    }
+    }*/
 
 
 
-    private IEnumerator GoByTheRoute(int routeNum)
+    private IEnumerator GoByTheRoute()
 
     {
-        coroutineAllowed = false;
 
-        Vector3 p0 = routes[routeNum].GetChild(0).position;
-        Vector3 p1 = routes[routeNum].GetChild(1).position;
-        Vector3 p2 = routes[routeNum].GetChild(2).position;
-        Vector3 p3 = routes[routeNum].GetChild(3).position;
-
-        p0 = RandomNavSphere(transform.position, radius, -1);
-        p1 = RandomNavSphere(transform.position, radius, -1);
-        p2 = RandomNavSphere(transform.position, radius, -1);
-        p3 = RandomNavSphere(transform.position, radius, -1);
+        Vector3 p0 = RandomNavSphere(transform.position, radius, -1);
+        Vector3 p1 = RandomNavSphere(transform.position, radius, -1);
+        Vector3 p2 = RandomNavSphere(transform.position, radius, -1);
+        Vector3 p3 = RandomNavSphere(transform.position, radius, -1);
 
         while (tParam < 1)
         {
@@ -60,7 +55,7 @@ public class FollowBezierCurve : MonoBehaviour
         }
 
         tParam = 0;
-        routeToGo += 1;
+        /*routeToGo += 1;
 
         if (routeToGo > routes.Length - 1)
 
@@ -68,9 +63,38 @@ public class FollowBezierCurve : MonoBehaviour
 
             routeToGo = 0;
 
+        }*/
+
+        StartCoroutine(GoByTheRoute(p3));
+    }
+
+    private IEnumerator GoByTheRoute(Vector3 startPosition)
+    {
+        Vector3 p0 = startPosition;
+        Vector3 p1 = RandomNavSphere(transform.position, radius, -1);
+        Vector3 p2 = RandomNavSphere(transform.position, radius, -1);
+        Vector3 p3 = RandomNavSphere(transform.position, radius, -1);
+
+        while (tParam < 1)
+        {
+            tParam += Time.deltaTime * speedModifier;
+            objectPosition = Mathf.Pow(1 - tParam, 3) * p0 + 3 * Mathf.Pow(1 - tParam, 2) * tParam * p1 + 3 * (1 - tParam) * Mathf.Pow(tParam, 2) * p2 + Mathf.Pow(tParam, 3) * p3;
+            transform.position = objectPosition;
+            yield return new WaitForEndOfFrame();
         }
 
-        coroutineAllowed = true;
+        tParam = 0;
+        /*routeToGo += 1;
+
+        if (routeToGo > routes.Length - 1)
+
+        {
+
+            routeToGo = 0;
+
+        }*/
+
+        StartCoroutine(GoByTheRoute(p3));
     }
 
     public static Vector3 RandomNavSphere(Vector3 origin, float dist, int layermask)
