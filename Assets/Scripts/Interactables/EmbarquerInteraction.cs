b0ba8d27943b9e149b45b63cf6960cafeb159d38
Raphahel <raphaelblanchard2002@gmail.com>
2024-04-05 16:42:43 +0200

    Small fixes

---
+++ Assets/Scripts/Interactables/EmbarquerInteraction.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class EmbarquerInteraction : AbstractInteractable
+{
+
+    [SerializeField]
+    private Image fadeToBlack;
+    
+    protected override void Interaction(InputAction.CallbackContext context)
+    {
+        
+    }
+    
+
+}
